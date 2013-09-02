e3012142ddfd16e26ed9f5bf4733013a0dd8875a
Mauro Bolis <bolismauro@gmail.com>
2013-09-02 21:36:00 +0200

    changed console appender (colors are awesome now)

--- builtinappender/console/index.js
+++ builtinappender/console/index.js
@@ -1,7 +1,9 @@
 /*jshint node:true, indent:2, white:true, laxcomma:true, undef:true, strict:true, unused:true, eqnull:true, camelcase: false */
 'use strict';
 
-var inspect = require('seye.js').inspector({ stream: null });
+var inspect = require('seye.js').inspector({ stream: null })
+  , chalk = require('chalk')
+  ;
 
 var COLOR_MAP = {
   'log'   : 'white'
@@ -17,7 +19,24 @@ var COLOR_MAP = {
 module.exports = function () {
   var log
     , _getLine
-    , _log_helper;
+    , _log_helper
+    , _generate_color;
+
+  _generate_color = function (str) {
+    var hash = 0
+      , c
+      , key;
+
+    if (str.length === 0) return hash;
+    for (var i = 0; i < str.length; i++) {
+      c = str.charCodeAt(i);
+      hash = ((hash<<5) - hash) + c;
+      hash = hash & hash; // Convert to 32bit integer
+    }
+    
+    key = Object.keys(COLOR_MAP)[hash % Object.keys(COLOR_MAP).length];
+    return COLOR_MAP[key];
+  };
 
   _getLine = function () {
     var e = new Error();
@@ -26,19 +45,19 @@ module.exports = function () {
   }; 
 
   _log_helper = function (level_name, channel_name, filename, color, line, msg) {
-    
+    var channel_name_color = _generate_color(channel_name);
+
     if (msg.indexOf('\n') > -1) {
       // If string is multiline call _log for each string
       msg.split('\n').forEach(function (row) {
         _log_helper(level_name, channel_name, filename, color, line, row);
       });
       
-    } else {
-      
+    } else {  
       console.log.apply(console, [
-          '['.grey.bold + channel_name.grey + ', '.grey + filename.grey + ':'.grey + line.grey + ']'.grey.bold
-        , (level_name + ':')[color].bold
-        , msg
+          chalk.bold.grey('[') + chalk[channel_name_color](channel_name + ', ') + chalk.blue(filename + ':' + line) + chalk.bold.grey(']')
+        , chalk[color](level_name + ':')
+        , msg 
         ].concat('')
       );
     }
